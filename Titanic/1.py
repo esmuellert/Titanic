4ded1f907da82d505802d6b24f9ecdd2316afc17
esmuellert <myn508@hotmail.com>
2018-09-17 17:56:29 +0800

    update 归一化

--- Titanic/1.py
+++ Titanic/1.py
@@ -3,8 +3,17 @@ import tensorflow as tf
 #import matplotlib.pyplot as plt
 
 # get data from train_1.csv
-tmp = np.genfromtxt("train_1.csv", dtype=np.str, delimiter=",")
-train_data = tmp[1:,2:]
+tmp = np.genfromtxt("train_1.csv", dtype=float, delimiter=",")
+
+tem = tmp[1:,2:]
+ave = np.mean(tem,axis = 0)*np.ones((891,6))
+ptp = np.ptp(tem,axis = 0)*np.ones((891,6))
+tem = np.subtract(tem,ave)
+tem = np.true_divide(tem,ptp)
+tmp[1:,2:] = tem
+
+train_data = tmp[1:601,2:]
+validation_data = tmp[601:,2:]
 train_lable = tmp[1:,1]
 train_lable = train_lable.reshape(891,1)
 
@@ -16,7 +25,7 @@ def add_layer(input,in_size,out_size):
     else:
         z = tf.matmul(input,theta)
         a = tf.sigmoid(z)
-        a_0 = tf.ones([891,1])
+        a_0 = tf.ones([600,1])
         a = tf.concat([a_0,a],1)
     return theta,a,z
 
@@ -33,7 +42,7 @@ theta_2,output,z_2 = add_layer(l1,13,1)
 # the error between prediction and real data
 J_theta = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = ys,logits = z_2))
 
-a = 0.013 # learning rate
+a = 0.1 # learning rate
 train_step = tf.train.GradientDescentOptimizer(a).minimize(J_theta)
 
 #initialize
@@ -44,9 +53,13 @@ sess.run(init)
 
 for i in range(100000):
     #training
-    sess.run(train_step,feed_dict={xs:train_data,ys:train_lable})
+    sess.run(train_step,feed_dict={xs:train_data[0:600],ys:train_lable[0:600]})
     
     if i%1000 == 0:
-        loss = sess.run(J_theta,feed_dict={xs:train_data,ys:train_lable})
-        print(loss)
+        loss = sess.run(J_theta,feed_dict={xs:train_data[0:600],ys:train_lable[0:600]})
+        print("J_theta:",loss)
+        theta1 = sess.run(theta_1)
+        theta2 = sess.run(theta_2)
+        #print(theta1,theta2,train_data[601:890])
+
 
