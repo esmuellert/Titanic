a0e09eb5188693a7b2956ec97300fa47affad696
esmuellert <myn508@hotmail.com>
2018-09-17 14:35:03 +0800

    delete matplotlib

--- Titanic/1.py
+++ Titanic/1.py
@@ -1,8 +1,8 @@
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-## get data from train_1.csv
+# get data from train_1.csv
 tmp = np.genfromtxt("train_1.csv", dtype=np.str, delimiter=",")
 train_data = tmp[1:,2:]
 train_lable = tmp[1:,1]
@@ -33,7 +33,7 @@ theta_2,output,z_2 = add_layer(l1,13,1)
 # the error between prediction and real data
 J_theta = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = ys,logits = z_2))
 
-a = 0.012 # learning rate
+a = 0.013 # learning rate
 train_step = tf.train.GradientDescentOptimizer(a).minimize(J_theta)
 
 #initialize
@@ -42,7 +42,7 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-for i in range(10000000):
+for i in range(100000):
     #training
     sess.run(train_step,feed_dict={xs:train_data,ys:train_lable})
     
